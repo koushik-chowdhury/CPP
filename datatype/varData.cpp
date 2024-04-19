0f992b7ff37249ff0332ffe8fbd981ca0b9101bc
Koushik Chowdhury <csakoushik@gmail.com>
2024-04-19 21:42:24 +0530

    cpp datatype

---
+++ datatype/varData.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // variable define
+    int age = 21; // integer dataype
+    char letter = 'A'; // character datatype
+    float pi = 3.14; // float datatype
+    bool isMale = true; // boolean datatype : returns true or false
+
+    cout << "Your age is : " << age << endl;
+    cout << "First alphabet is : " << letter << endl;
+    cout << "Value of Pi is : " << pi << endl;
+    cout << "Your Gender is Male [ 1 = true & 0 = false ] : " << isMale << endl;
+
+    return 0;
+}
