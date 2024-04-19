16acb7b45c3873b2dcb030f9eb2bd12df61d2c92
Koushik Chowdhury <csakoushik@gmail.com>
2024-04-19 22:03:09 +0530

    user input in cpp

---
+++ basic/input_output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // variable define
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age; // taking input from user
+    cout << "Your age is: " << age << endl; // displaying the result
+}
