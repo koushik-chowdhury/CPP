fc2302227581f00d5a0ab84f51ee58398c9056db
Koushik Chowdhury <csakoushik@gmail.com>
2024-04-28 18:00:33 +0530

    modify logic

--- gfg solution/leapYear.cpp
+++ gfg solution/leapYear.cpp
@@ -10,13 +10,9 @@ int main()
     cout << "Enter year: ";
     cin >> year;
 
-    if (year % 100 == 0 && year % 400 == 0)
+    if(year % 400 == 0  || year % 100 != 0 && year % 4 == 0)
     {
         cout << year << " is leap Year";
-    }
-    else if (year % 100 != 0 && year % 4 == 0)
-    {
-        cout << year << " is leap year";
     }
     else
     {
