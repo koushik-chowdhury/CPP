3331f5e16582096a58c14f30e29a86585325cd55
Koushik Chowdhury <csakoushik@gmail.com>
2024-06-10 22:14:15 +0530

    pointer

---
+++ class/class.cpp
@@ -0,0 +1,23 @@
+// pointer. Date: 10-06-24
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 5;
+    // int* ptr = &a;
+    // cout << a << endl;
+    // cout << &a << endl;
+    // // cout << *a << endl;
+    // cout << ptr << endl;
+    // cout << &ptr << endl;
+    // cout << *ptr << endl;
+    // cout << ptr + 1 << endl;
+    // cout << *ptr / 2 << endl;
+
+    int arr[5] = {10,20,30,40,50};
+    int* ptr = arr;
+    cout << ptr << endl;
+    return 0;
+}
